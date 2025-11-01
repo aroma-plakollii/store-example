faf4b77b36088431c9d5bd01fea97f2d25bf5d07
Aroma Plakolli <plakolli.aroma@outlook.com>
2025-11-01 10:46:48 +0100

    finished context

--- src/App.js
+++ src/App.js
@@ -1,82 +1,14 @@
-import { useState } from "react";
-
 import Header from "./components/Header";
 import Shop from "./components/Shop";
-import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import { CartContext } from "./store/CartContext.jsx";
+import CartContextProvider from "./store/CartContext.jsx";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  const cartCtx = {
-    items: shoppingCart.items,
-    addItemsToCart: handleAddItemToCart,
-    updateCartItemQuantity: handleUpdateCartItemQuantity
-  }
 
   return (
-    <CartContext.Provider value={cartCtx}>
+    <CartContextProvider>
       <Header />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </ CartContext.Provider>
+      <Shop />
+    </CartContextProvider>
   );
 }
 
