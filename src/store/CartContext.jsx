0827a63b7b11bd379cdc23312c6a852069f84133
Aroma Plakolli <plakolli.aroma@outlook.com>
2025-11-03 15:57:20 +0100

    added reducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useReducer} from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products";
+import { type } from "@testing-library/user-event/dist/type";
 
 export const CartContext = createContext({
     items: [],
@@ -7,69 +8,86 @@ export const CartContext = createContext({
     udateCartItemQuantity: () => {}
 })
 
-export default function CartContextProvider({children}){
-    const [shoppingCart, setShoppingCart] = useState({
-        items: [],
-      });
-    
-    function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-        const updatedItems = [...prevShoppingCart.items];
+const shoppingCartReducer = (state, action) => {
+    if(action.type === 'ADD_ITEM'){
+        const updatedItems = [...state.items];
 
         const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
+        (cartItem) => cartItem.id === action.payload
         );
         const existingCartItem = updatedItems[existingCartItemIndex];
 
         if (existingCartItem) {
-        const updatedItem = {
-            ...existingCartItem,
-            quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity + 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
         } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-            id: id,
-            name: product.title,
-            price: product.price,
-            quantity: 1,
-        });
+            const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+            updatedItems.push({
+                id: action.payload,
+                name: product.title,
+                price: product.price,
+                quantity: 1,
+            });
         }
 
         return {
-        items: updatedItems,
+            items: updatedItems,
         };
-    });
     }
 
-    function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-        const updatedItems = [...prevShoppingCart.items];
+    if(action.type === 'UPDATE_CART'){
+        const updatedItems = [...state.items];
         const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
+        (item) => item.id === action.payload.productId
         );
 
         const updatedItem = {
-        ...updatedItems[updatedItemIndex],
+            ...updatedItems[updatedItemIndex],
         };
 
-        updatedItem.quantity += amount;
+        updatedItem.quantity += action.payload.amount;
 
         if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
+            updatedItems.splice(updatedItemIndex, 1);
         } else {
-        updatedItems[updatedItemIndex] = updatedItem;
+            updatedItems[updatedItemIndex] = updatedItem;
         }
 
         return {
-        items: updatedItems,
+            items: updatedItems,
         };
-    });
+    }
+
+    return state;
+}
+
+export default function CartContextProvider({children}){
+    const [shoppingCartState, shoppingCartDispatch] = useReducer(shoppingCartReducer, {
+        items: [],
+      });
+    
+    function handleAddItemToCart(id) {
+        shoppingCartDispatch({
+            type: 'ADD_ITEM',
+            payload: id
+        })
+    }
+
+    function handleUpdateCartItemQuantity(productId, amount) {
+        shoppingCartDispatch({
+            type: 'UPDATE_CART',
+            payload: {
+                productId,
+                amount
+            }
+        })
     }
 
     const cartCtx = {
-    items: shoppingCart.items,
+    items: shoppingCartState.items,
     addItemsToCart: handleAddItemToCart,
     updateCartItemQuantity: handleUpdateCartItemQuantity
     }
